1f79ce493aefe6b7b8aaca948cf4501352270880
Doppon <zaxpkuraudo7777@gmail.com>
2019-07-18 19:42:31 +0900

    code_idを大きい順にソートする関数を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.sort_by_code_id
   end
 
   # GET /books/1
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  scope :sort_by_code_id, -> { order(id: :desc).limit(100) }
 end
