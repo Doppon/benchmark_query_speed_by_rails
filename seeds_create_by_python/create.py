e1d190ab86e88d3042d7d5c6c9aa488bcec94ab4
Doppon <zaxpkuraudo7777@gmail.com>
2019-07-18 18:53:22 +0900

    seedを生成するコードを追加

--- seeds_create_by_python/create.py
+++ seeds_create_by_python/create.py
@@ -1,7 +1,22 @@
 import random
 
 path = 'seed.txt'
-data = int( random.uniform(1.0, 50000.0) )
+
+msg_header  = "Book.create!(\n"
+msg_header += "  [\n"
+msg_body    = ""
+msg_footer  = "  ]\n"
+msg_footer += ")"
 
 with open(path, mode='w') as f:
-    f.write(str(data))
+    for idx in range(10):
+        name    = "テストユーザー"
+        code_id = int( random.uniform(1.0, 50000.0) )
+
+        msg_body += "    {\n"
+        msg_body += "      name: '{}_{}',\n".format(name, idx)
+        msg_body += "      code_id: {}\n".format(code_id)
+        msg_body += "    },\n"
+
+    msg = msg_header + msg_body + msg_footer
+    f.write(msg)
